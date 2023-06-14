74e32eb260c45ede12e7c512b09097a3e6f56d5f
@hexlet/cli <support@hexlet.io>
2023-06-14 17:37:52 +0400

    submit java-oop-ru/classes-and-objects

---
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Point.java
@@ -0,0 +1,27 @@
+package exercise;
+
+public class Point {
+    private int X;
+    private int Y;
+
+    public Point(int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+
+    public Point() {
+        X = 0;
+        Y = 0;
+    }
+
+    public int getX() {
+        return this.X;
+    }
+
+
+    public int getY() {
+        return this.Y;
+    }
+
+}
+// END
---
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -0,0 +1,29 @@
+package exercise;
+
+// BEGIN
+public class Segment {
+    private Point beginPoint;
+    private Point endPoint;
+
+    private Point midPoint;
+    public Segment(Point beginPoint, Point endPoint) {
+        if (beginPoint == null || endPoint == null) {
+            throw new IllegalArgumentException("Cannot have null point(s).");
+        }
+        this.beginPoint = beginPoint;
+        this.endPoint = endPoint;
+    }
+    public Point getBeginPoint() {
+        return this.beginPoint;
+    }
+    public Point getEndPoint() {
+        return this.endPoint;
+    }
+
+    public Point getMidPoint() {
+        int midX = (this.beginPoint.getX() + this.endPoint.getX())/2;
+        int midY = (this.beginPoint.getY() + this.endPoint.getY())/2;
+        return new Point(midX, midY);
+    }
+}
+// END
