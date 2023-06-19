12eb56635a482decdc190f6f692407959ceb13c5
@hexlet/cli <support@hexlet.io>
2023-06-19 19:24:57 +0400

    submit java-oop-ru/subtyping

---
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -0,0 +1,38 @@
+package exercise;
+
+import java.util.Map;
+
+// BEGIN
+class FileKV implements KeyValueStorage {
+    private String filePath;
+    private Map<String, String> map;
+
+    FileKV(String filePath, Map<String, String> map) {
+        this.filePath = filePath;
+        this.map = map;
+        Utils.writeFile(filePath, map.toString());
+        Utils.serialize(map);
+    }
+    @Override
+    public void set(String key, String value) {
+        map.put(key, value);
+        Utils.writeFile(filePath, map.toString());
+    }
+
+    @Override
+    public void unset(String key) {
+        map.remove(key);
+        Utils.writeFile(filePath, map.toString());
+    }
+
+    @Override
+    public String get(String key, String defaultValue) {
+        return map.getOrDefault(key, defaultValue);
+    }
+
+    @Override
+    public Map<String, String> toMap() {
+        return map;
+    }
+}
+// END
