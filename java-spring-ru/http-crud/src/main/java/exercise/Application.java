9b364eee55e85028ccd3f6db3d17867fdab087d7
@hexlet/cli <support@hexlet.io>
2024-01-17 18:51:51 +0400

    submit java-spring-ru/http-crud

--- java-spring-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-ru/http-crud/src/main/java/exercise/Application.java
@@ -27,18 +27,18 @@ public class Application {
     }
 
     // BEGIN
-    @GetMapping("/posts") // Список страниц
+    @GetMapping("/posts")
     public List<Post> index(@RequestParam(defaultValue = "10") Integer limit) {
         return posts.stream().limit(limit).toList();
     }
 
-    @PostMapping("/posts") // Создание страницы
+    @PostMapping("/posts")
     public Post create(@RequestBody Post post) {
         posts.add(post);
         return post;
     }
 
-    @GetMapping("/posts/{id}") // Вывод страницы
+    @GetMapping("/posts/{id}")
     public Optional<Post> show(@PathVariable String id) {
         var post = posts.stream()
                 .filter(p -> p.getId().equals(id))
@@ -46,7 +46,7 @@ public class Application {
         return post;
     }
 
-    @PutMapping("/posts/{id}") // Обновление страницы
+    @PutMapping("/posts/{id}")
     public Post update(@PathVariable String id, @RequestBody Post data) {
         var maybePage = posts.stream()
                 .filter(p -> p.getId().equals(id))
@@ -60,7 +60,7 @@ public class Application {
         return data;
     }
 
-    @DeleteMapping("/posts/{id}") // Удаление страницы
+    @DeleteMapping("/posts/{id}")
     public void destroy(@PathVariable String id) {
         posts.removeIf(p -> p.getId().equals(id));
     }
